a8859e8e61a7019ebfa932d74542573f9903efbc
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2023-04-28 21:37:40 -0500

    Add initial unreal project

---
+++ deLivery/Source/deLivery/deLivery.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "deLivery.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, deLivery, "deLivery" );
---
+++ deLivery/Source/deLivery/deLiveryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "deLiveryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DELIVERY_API AdeLiveryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
